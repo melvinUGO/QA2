9bc0716e645101153873ced0e2385d71363188af
Melvin Ugochukwu <ugochukwumelvin@gmail.com>
2023-11-06 10:42:33 +0100

    six commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const { ObjectID } = require("mongodb");
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
@@ -39,6 +40,18 @@ myDB(async (client) => {
       message: "Please login",
     });
   });
+
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
 
   // Serialization and deserialization here...
 
